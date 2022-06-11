2b92f580057027cf126a62e6b114bace32234b4f
Bo Lundehave Nyby <201906213@post.au.dk>
2022-06-11 18:16:01 +0200

    Setup C# project and have basic logging working

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CityGrowthSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testeren!");
+        }
+    }
+}
