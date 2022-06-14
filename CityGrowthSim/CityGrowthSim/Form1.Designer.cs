27b29dfc4a856ddcdd61568f79f5230da2ce5406
Bo Lundehave Nyby <201906213@post.au.dk>
2022-06-14 21:49:19 +0200

    Setup basic project. A Windows Forms App with Visual Studio 2022 Community

---
+++ CityGrowthSim/CityGrowthSim/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CityGrowthSim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickBtn = new System.Windows.Forms.Button();
+            this.hwLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clickBtn
+            // 
+            this.clickBtn.Location = new System.Drawing.Point(352, 173);
+            this.clickBtn.Name = "clickBtn";
+            this.clickBtn.Size = new System.Drawing.Size(75, 23);
+            this.clickBtn.TabIndex = 0;
+            this.clickBtn.Text = "CLICK!";
+            this.clickBtn.UseVisualStyleBackColor = true;
+            this.clickBtn.Click += new System.EventHandler(this.clickBtn_Click);
+            // 
+            // hwLbl
+            // 
+            this.hwLbl.AutoSize = true;
+            this.hwLbl.Location = new System.Drawing.Point(370, 223);
+            this.hwLbl.Name = "hwLbl";
+            this.hwLbl.Size = new System.Drawing.Size(35, 13);
+            this.hwLbl.TabIndex = 1;
+            this.hwLbl.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hwLbl);
+            this.Controls.Add(this.clickBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickBtn;
+        private System.Windows.Forms.Label hwLbl;
+    }
+}
+
---
+++ CityGrowthSim/CityGrowthSim/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CityGrowthSim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clickBtn_Click(object sender, EventArgs e)
+        {
+            hwLbl.Text = "shizzle my nizzle, iz workin'";
+        }
+    }
+}
