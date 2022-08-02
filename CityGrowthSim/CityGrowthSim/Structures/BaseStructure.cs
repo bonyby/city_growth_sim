ca43f6d03c02cc97f2f9d1c5e19d25a4cfed4656
Bo Lundehave Nyby <201906213@post.au.dk>
2022-08-02 21:28:24 +0200

    Forgot to add some files

---
+++ CityGrowthSim/CityGrowthSim/Structures/BaseStructure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityGrowthSim.Structures
+{
+    internal class BaseStructure : IStructure
+    {
+        Point[] corners;
+        public Point[] Corners
+        {
+            get => corners;
+            set
+            {
+                corners = value;
+                for (int i = 0; i < corners.Length; i++)
+                {
+                    if (corners[i].X < 0) { corners[i].X = 0; }
+                    if (corners[i].Y < 0) { corners[i].Y = 0; }
+                }
+            }
+        }
+
+        public Point Position => throw new NotImplementedException();
+
+        public int BoundingWidth => throw new NotImplementedException();
+
+        public int BoundingHeight => throw new NotImplementedException();
+    }
+}
