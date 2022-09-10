c752e9f436c5f1b6cf75b531f3358e991aa70270
Bo Lundehave Nyby <201906213@post.au.dk>
2022-09-10 16:07:36 +0200

    Added a base wall structure to build inherit when creating wall variants.

---
+++ CityGrowthSim/CityGrowthSim/City/Structures/Walls/BaseWall.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityGrowthSim.City.Structures.Walls
+{
+    internal abstract class BaseWall : BaseStructure
+    {
+        protected BaseWall(Point position, float width) : base(position)
+        {
+            this.Width = width;
+        }
+
+        protected BaseWall(Point position, Point[] corners, float width) : base(position, corners)
+        {
+            this.Width = width;
+        }
+
+        public float Width { get; }
+    }
+}
